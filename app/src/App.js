53e213cfcb94044ff9923f9285b866ac1946e8ea
Carol Enache <carol.enache@loveholidays.com>
2019-09-18 20:22:53 +0100

    config the workspaces to export components

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Button from 'button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Button>hello world</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
