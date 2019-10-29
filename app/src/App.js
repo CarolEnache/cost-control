6e32c1f4219eb0ff5da7ccbd113b8eff93f5870b
CarolEnache <enache.carol@gmail.com>
2019-10-29 00:47:35 +0000

    automate build for all workspaces

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import Button from 'button';
 import Input from 'input';
+import Select from 'select'
 import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
 
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <Button name='add'>
         <AddIcon />
       </Button>
       <Input placeholder="Hello world!" id='someId' type='text' label='Repice name' />
+      <Select placeholder="Hello world!" label='Ingredient name' />
     </div>
   );
 }
