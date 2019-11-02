9698eb25785a52b6fc0dda6167db54b5f1514453
CarolEnache <enache.carol@gmail.com>
2019-11-02 19:12:50 +0000

    initialize Firebase and read from the database

--- app/src/App.js
+++ app/src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState }  from 'react';
+
+import firebase from './firebase';
 
 import Button from '@component-library/button';
 import Input from 'input';
@@ -9,7 +11,18 @@ import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
 import './App.css';
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const db = firebase.firestore();
+      const docRef = await db.collection('ingredients_list').get();
+      docRef.docs.map(doc => console.log(doc.data()))
+      setIngredients(docRef.docs.map(doc => doc.data()))
+    }
+    fetchData()
+  }, []);
+  console.log(ingredients);
   return (
     <div className="App">
       <Button name='add'>
---
+++ app/src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAs1K3BKgkcdLShgN8rRbP44sJPH-VnIPI',
+  authDomain: 'cost-control-46ad2.firebaseapp.com',
+  databaseURL: 'https://cost-control-46ad2.firebaseio.com',
+  projectId: 'cost-control-46ad2',
+  storageBucket: 'cost-control-46ad2.appspot.com',
+  messagingSenderId: '433695748792',
+  appId: '1:433695748792:web:16e480a23ff13781affa4d',
+  measurementId: 'G-XNK5TQ5F0H'
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase;
