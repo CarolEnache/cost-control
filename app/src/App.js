07a2ede144f693a84399e397325a252f05a198b2
CarolEnache <enache.carol@gmail.com>
2019-11-04 22:04:22 +0000

    write on the Database

--- app/src/App.js
+++ app/src/App.js
@@ -2,21 +2,18 @@ import React, { useEffect, useState }  from 'react';
 
 import firebase from './firebase';
 
-import Button from 'button';
 import Input from 'input';
-import Select from 'select'
-
-import { ReactComponent as AddIcon } from 'assets/icons/add.svg';
 
 import './App.css';
 
 function App() {
   const [ingredients, setIngredients] = useState([]);
+  const collection = 'ingredients_list';
+  const db = firebase.firestore();
 
   useEffect(() => {
     const fetchData = async () => {
-      const db = firebase.firestore();
-      const docRef = await db.collection('ingredients_list').get();
+      const docRef = await db.collection(collection).get();
       docRef.docs.map(doc => console.log(doc.data()))
       setIngredients(docRef.docs.map(doc => doc.data()))
     }
@@ -24,18 +21,67 @@ function App() {
   }, []);
   console.log(ingredients);
 
-  const handleIngredientSubmition = (event) => {
-    event.preventDefault();
-    console.log('submited')
+  const createFirestoreItem = (collection, item) => {
+    try {
+      db.collection(collection).add(item);
+    }
+    catch(e) {
+      console.error(e);
+    }
+  };
+
+  const [ingredientName, setIngredientName] = useState('');
+  const [ingredientYield, setIngredientYield] = useState(0);
+  const [ingredientPrice, setIngredientPrice] = useState(0);
+  const handleChange = (e) => {
+    switch(e.target.id) {
+      case 'name':
+        return setIngredientName(e.target.value)
+      case 'yield':
+        return setIngredientYield(e.target.value)
+      case 'price':
+        return setIngredientPrice(e.target.value)
+      default:
+        return null
+    }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const item = {
+      ingredientName,
+      ingredientYield,
+      ingredientPrice
+    }
+    createFirestoreItem(collection, item);
+}
   return (
     <div className="App">
-      <Button name='add'>
-        <AddIcon />
-      </Button>
-      <Input placeholder="Hello world!" id='someId' type='text' label='Repice name' />
-      <Select submit={handleIngredientSubmition} />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <Input
+          placeholder='minced meat'
+          id='name'
+          type='text'
+          label='Ingredient name'
+          onChange={(e) => handleChange(e)}
+        />
+        <Input
+          placeholder='1000'
+          id='yield'
+          type='number'
+          label='Package yield'
+          onChange={(e) => handleChange(e)}
+
+        />
+        <Input
+          placeholder='25.50'
+          id='price'
+          type='number'
+          label='Package price'
+          onChange={(e) => handleChange(e)}
+        />
+        <Input type='submit' value='submit' />
+      </form>
     </div>
   );
 }
--- component-library/input/src/index.js
+++ component-library/input/src/index.js
@@ -4,7 +4,7 @@ import { StyledInput, StyledLabel, StyledInputWrapper } from './styled';
 const Input = ({ id, placeholder, type, label, name, onChange, value }) => (
   <StyledInputWrapper name={name}>
       <StyledLabel htmlFor={id} name={name}>{label}</StyledLabel>
-    <StyledInput id={id} placeholder={placeholder} name={name} type={type} onChange={onChange} value={value}/>
+    <StyledInput id={id} placeholder={placeholder} name={name} type={type} onChange={onChange} value={value} step='any' required/>
   </StyledInputWrapper>
 )
 
